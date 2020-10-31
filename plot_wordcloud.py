f357a745f6c61ca4847ff5800ee611615f99d416
Tom <abaotom14@gmail.com>
2020-10-31 08:23:10 +0800

    add get json data

--- plot_wordcloud.py
+++ plot_wordcloud.py
@@ -1,14 +1,17 @@
-from generate_from_frequency import generate_word_cloud, plot_word_cloud
+import json
 
+from generate_from_frequency import generate_word_cloud, plot_word_cloud
 from constants import width, height, dpi, figsize, color
+
+  
+def get_data(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        return data
 
 
 if __name__ == "__main__":
-    data = {
-        'One': 7,
-        'Two': 3,
-        'Three': 2,
-        'Four': 1
-    }
+    filename = 'input/sample.json'
+    data = get_data(filename)
     wc = generate_word_cloud(width, height, data)
     plot_word_cloud(figsize, dpi, wc)
